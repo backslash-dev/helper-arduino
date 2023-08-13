fe1395121a52b72e9a61bbe052dd8f2ce5f399a2
Jaemin Lee <ljm005302@gmail.com>
2023-08-14 04:40:59 +0900

    Add : wifi module

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,16 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include "certs.h"
+
+#ifndef STASSID
+#define STASSID "이재민의 iPhone"
+#define STAPSK "1234qwer"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+X509List cert(cert_DigiCert_Global_Root_CA);
 
 int pulseSensorPurplePin = A0; // 심박센서 핀
 int signal; // 실제 감지되는 심박수 값
@@ -13,29 +26,88 @@ void setup() {
   pinMode(peizoPin, OUTPUT); 
   pinMode(emergencyButtonPin, INPUT_PULLUP);
 
-  Serial.begin(9600);    
- 
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set time via NTP, as required for x.509 validation
+  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+  Serial.print("Waiting for NTP time sync: ");
+  time_t now = time(nullptr);
+
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("Connecting to ");
+  Serial.println(github_host);
+
+  Serial.printf("Using certificate: %s\n", cert_DigiCert_Global_Root_CA);
+  client.setTrustAnchors(&cert);
+
+  if (!client.connect(github_host, github_port)) {
+    Serial.println("Connection failed");
+    return;
+  }
+
+  String url = "/repos/esp8266/Arduino/commits/master/status";
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + github_host + "\r\n" + "User-Agent: BuildFailureDetectorESP8266\r\n" + "Connection: close\r\n\r\n");
+
+  Serial.println("Request sent");
+  while (client.available()) {
+    String line = client.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("Headers received");
+      break;
+    }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+    Serial.println("esp8266/Arduino CI successful!");
+  } else {
+    Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("Reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("Closing connection");
 }
  
 void loop() {
-  // signal = analogRead(pulseSensorPurplePin);
+  signal = analogRead(pulseSensorPurplePin);
   // Serial.println(signal);
- 
-  // if(signal > Threshold){                  
-  //    digitalWrite(ledPin, HIGH);
-  // } else {
-  //    digitalWrite(ledPin, LOW);               
-  // }
 
   if (digitalRead(emergencyButtonPin) == LOW) {
-    digitalWrite(ledPin, LOW);
     digitalWrite(peizoPin, HIGH);
   } else {
-    digitalWrite(ledPin, HIGH);
     digitalWrite(peizoPin, LOW);
-    
   }
-  // delay(10);
+  delay(10);
 
 
 }
